b356e3af52fe901461d04e49119d728bccc5f6ab
Александр <0999353523ak@gmail.com>
2024-06-13 12:11:31 +0300

    make build

---
+++ index.3d7c2665.js
@@ -0,0 +1,2 @@
+for(var e=document.body.firstElementChild,l=0;l<e.rows.length;l++){var o=e.rows[l],r=o.cells[1].cloneNode(!0);o.insertBefore(r,o.cells[o.cells.length-1])}
+//# sourceMappingURL=index.3d7c2665.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+// write your code here
+const table = document.body.firstElementChild;
+for(let i = 0; i < table.rows.length; i++){
+    const row = table.rows[i];
+    const coppiedCell = row.cells[1].cloneNode(true);
+    row.insertBefore(coppiedCell, row.cells[row.cells.length - 1]);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
